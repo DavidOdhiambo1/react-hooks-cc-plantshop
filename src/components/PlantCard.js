efd3df85463fe56aa9ec3acacc42cd0bb0e74b0d
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-12-03 21:38:10 +0300

    first commit with core deliverables

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({plant}) {
+
+  const [inStock, setInStock]= useState(true)
+
+  function handleInstock (){
+    setInStock(false)
+  }
+  
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      {inStock ? (
+        <button className="primary" onClick = {handleInstock}>In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(()=>{
+      fetch("http://localhost:6001/plants")
+        .then((r) => r.json())
+        .then(data => setPlants(data))
+  }, [])
+
+  function addnewPlant(newPlant) {
+    const updatedPlants = [...plants, newPlant]
+    setPlants(updatedPlants);
+  }
+
+  function handleSearchChange(newSearch) {
+    setSearch(newSearch)
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddNewPlant={addnewPlant}/>
+      <Search search={search} onSearchChange={handleSearchChange}/>
+      <PlantList plants={plants} search={search}/>
     </main>
   );
 }
